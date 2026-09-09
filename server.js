edf8ac717542e3100bcd707e015c5c8e76eba3c7
Aditya Bhandekar <adityabhandekar77@gmail.com>
2026-09-09 23:10:53 +0530

    Connect Express API to PostgreSQL

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const pool = require("./db");
+
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Personal Digital Timeline API");
+});
+
+app.get("/api/timeline", async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM timeline_entries ORDER BY created_at DESC"
+        );
+
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Database query failed"
+        });
+    }
+});
+
 
 const PORT = 3000;
 
