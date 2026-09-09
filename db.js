e64f92b82985fe10b3184b0c0a979a8b200dc20e
Aditya Bhandekar <adityabhandekar77@gmail.com>
2026-09-09 23:16:41 +0530

    Clean up API and database configuration

--- db.js
+++ db.js
@@ -2,11 +2,6 @@ const { Pool } = require("pg");
 
 require("dotenv").config();
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -14,7 +9,5 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
 });
-
-
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const pool = require("./db");
 
-
 const app = express();
 
 app.use(express.json());
@@ -19,30 +18,15 @@ app.get("/api/timeline", async (req, res) => {
         res.json(result.rows);
     } catch (error) {
         console.error(error);
+
         res.status(500).json({
             error: "Database query failed"
         });
     }
 });
 
-
 const PORT = 3000;
 
-app.get("/", (req, res) =>{
-    res.send("Personal Digital Timeline API is running! ");
-});
-
-app.get("/api/timeline", (req, res) => {
-    res.json([
-        {
-            id: 1,
-            title: "Started Personal Digital Timeline",
-            type: "project",
-            date: "2026-09-09"
-        }
-    ]);
-});
-
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}');
+    console.log(`Server running on http://localhost:${PORT}`);
 });
