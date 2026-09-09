6d1930b214573835c78660d60f61287786680e7d
Aditya Bhandekar <adityabhandekar77@gmail.com>
2026-09-10 01:26:15 +0530

    Add PUT timeline endpoint

--- server.js
+++ server.js
@@ -85,6 +85,47 @@ app.get("/api/timeline/:id", async (req, res) => {
     }
 });
 
+app.put("/api/timeline/:id", async(req, res) =>{
+
+    try{
+
+    
+    const { id } = req.params;
+    const { title, description, type } = req.body;
+
+if (isNaN(id)) {
+            return res.status(400).json({
+                error: "ID must be a valid number"
+            });
+        }
+
+if (!title || !description || !type) {
+            return res.status(400).json({
+                error: "title, description, and type are required"
+            });
+        }
+
+        const result = await pool.query(
+    "UPDATE timeline_entries SET title = $1, description = $2, type = $3 WHERE id = $4 RETURNING *",
+    [title, description, type, id]
+);
+if(result.rows.length === 0){
+
+    return res.status(404).json({
+                error: "Timeline entry not found"
+            });
+
+}
+
+res.status(200).json(result.rows[0]);
+} catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Database query failed"
+        });
+    }
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
